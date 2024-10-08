0033c60b7b1f301b83e1987eda90dcf414a298c4
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-10-08 15:02:55 +0530

    fir

---
+++ place.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class place {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String place1=sc.nextLine();
+        String place2=sc.nextLine();
+        String place3=sc.nextLine();
+        String place4=sc.nextLine();
+        String place5=sc.nextLine();
+
+        System.out.print(place1 + " ," + place2 + " ," + place3 + " ," + place4 + " ," + place5);
+    }
+}
