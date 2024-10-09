569314c4ce557cd4445b6fd7d82f1a71093c5fa1
rhythmmunjal11 <munjalrhythm0002@gmail.com>
2024-10-09 14:28:25 +0530

    leapYear code

---
+++ javaProgram/leapYear.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class leapYear{
+
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		
+		if(((year%400 == 0) && (year%4==0)) || (year%100 != 0)){
+				
+				System.out.print("leap year");
+}
+
+		else{
+			System.out.print("no");
+}
+		
+
+
+	}
+
+
+}
